e0cc7e2003bd07bff0212c9545928451d59eade7
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2021-10-26 22:53:56 +0300

    Add game_over

---
+++ My project (1)/Assets/YouLoseScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouLoseScreen : MonoBehaviour
+{
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("map");
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
+
